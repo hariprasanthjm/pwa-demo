6ef828887b1ff154b2209991f684b0a0cc7c3dca
hariprasanthjm <hari92prasanth@gmail.com>
2017-12-16 22:35:42 +0530

    Implemented SideNav and Alignement issues fixed

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(-100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
+  menuState = 'out';
 
   constructor() { }
 
   ngOnInit() {
+  }
+  toggleMenu() {
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
   }
 
 }
