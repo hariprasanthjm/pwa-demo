e0ab592c8a44891c2d5f4e963fe74b49b9eb8b6d
hariprasanthjm <hari92prasanth@gmail.com>
2017-12-17 10:30:36 +0530

    fixes in auth guard

--- src/app/shared/guards/secure-auth.guard.ts
+++ src/app/shared/guards/secure-auth.guard.ts
@@ -13,7 +13,8 @@ export class SecureAuthGuard implements CanActivate {
         }
 
         // not logged in so redirect to login page with the return url
-        this.router.navigate(['/auth'], { queryParams: { returnUrl: state.url }});
+        state.url.length === 1 ? this.router.navigate(['/auth']) :
+        this.router.navigate(['/auth'], { queryParams: { returnUrl: state.url } });
         return false;
     }
 }
